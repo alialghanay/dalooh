eab908ebfca17c1b4309e7b62d5d3f411b32b4d9
ali m alghanay <kingbad1725@gmail.com>
2025-03-21 23:28:53 +0200

    make it responsive for small devices

--- src/home-page/components/carouesl/carousel.js
+++ src/home-page/components/carouesl/carousel.js
@@ -1,22 +1,36 @@
-import { Splide } from '@splidejs/react-splide';
-import './carousel.css';
-import '@splidejs/react-splide/css';
-import Slider from './slider';
-import svgU from './Frameu.svg';
-import svgD from './Framed.svg';
+import { Splide } from "@splidejs/react-splide";
+import "./carousel.css";
+import "@splidejs/react-splide/css";
+import Slider from "./slider";
+import svgU from "./Frameu.svg";
+import svgD from "./Framed.svg";
 
-const dir = require('./img/index');
+const dir = require("./img/index");
 
 function Carousel(props) {
-    return (
-        <div id="carouesl">
-            <Splide aria-label="carousel-heading" tag="section" data-splide='{"type":"loop","perPage":3}'>
-                {dir().map(x => <Slider>{x}</Slider>)}
-            </Splide>
-            <img src={svgU} alt='' id='car-svgU'  className='car-svg'/>
-            <img src={svgD} alt='' id='car-svgD' className='car-svg'/>
-        </div>
-    )
+  return (
+    <div id="carouesl">
+      <Splide
+        aria-label="carousel-heading"
+        tag="section"
+        options={{
+          type: "loop",
+          perPage: 3,
+          breakpoints: {
+            600: {
+              perPage: 1,
+            },
+          },
+        }}
+      >
+        {dir().map((x) => (
+          <Slider>{x}</Slider>
+        ))}
+      </Splide>
+      <img src={svgU} alt="" id="car-svgU" className="car-svg" />
+      <img src={svgD} alt="" id="car-svgD" className="car-svg" />
+    </div>
+  );
 }
 
 export default Carousel;
